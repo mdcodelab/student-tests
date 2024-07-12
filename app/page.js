10ee6f874d8c44dd837fadfb16d18566cf6cd264
miha11-hash <d_mihaela@msn.com>
2024-07-13 02:54:40 +0300

    HomePage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
-
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
 
 function HomePage() {
   return (
-    <div>
-      Home Page
+    <div className="h-screen w-screen flex justify-center items-center background-div">
+      <div className="max-w-50 auto bg-slate-400 p-4 rounded">
+        <h1 className="text-center text-2xl font-semibold pb-6 tracking-wide">
+          Test
+        </h1>
+        <h1 className="text-center text-2xl font-semibold pb-6 tracking-wide">
+          Management Financiar
+        </h1>
+        <Link href="/profile">
+          <Button className="mx-auto block flex text-xl racking-wide hover:bg-green-600 transition duration-500">
+            <Loader2 className="mr-2" animate-spin /> Start Test
+          </Button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HomePage
