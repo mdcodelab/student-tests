5bf80838d0cb9f118ed7270126ac38f68ba27756
miha11-hash <d_mihaela@msn.com>
2024-07-13 21:32:28 +0300

    DropdownLinks

--- components/ui/LinksDropdown.jsx
+++ components/ui/LinksDropdown.jsx
@@ -1,11 +1,30 @@
-
+import {
+  DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem} from "@/components/ui/dropdown-menu";
+import { Button } from "./button";
+import { AlignLeft } from "lucide-react";
+import Link from "next/link";
+import { FaRegNoteSticky } from "react-icons/fa6";
+import { LuGoal } from "react-icons/lu";
+import { PiStudent } from "react-icons/pi";
 
 function LinksDropdown() {
   return (
-    <div>
-      LinksDropdown
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="icon">
+        <AlignLeft></AlignLeft>
+        <span className="sr-only">Toggle Links</span>
+        </Button>
+      </DropdownMenuTrigger>
+        
+        <DropdownMenuContent className="w-52 lg:hidden" align="start" sideOffset={25}>
+            <DropdownMenuItem><Link href="/profile/test" className="flex align-center gap-x-2"><FaRegNoteSticky></FaRegNoteSticky> Test</Link></DropdownMenuItem>
+            <DropdownMenuItem><Link href="/profile/results" className="flex align-center gap-x-2"><LuGoal></LuGoal> Rezultate</Link></DropdownMenuItem>
+            <DropdownMenuItem><Link href="/profile/students" className="flex align-center gap-x-2"><PiStudent></PiStudent> Studenti</Link></DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
-export default LinksDropdown
+
+export default LinksDropdown;
--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -5,13 +5,15 @@ import { UserButton } from "@clerk/nextjs";
 function Navbar() {
   return (
     <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
-      <LinksDropdown></LinksDropdown>
+      <div>
+        <LinksDropdown></LinksDropdown>
+      </div>
       <div className="flex items-center gap-x-4">
         <ThemeToggle></ThemeToggle>
         <UserButton></UserButton>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar
