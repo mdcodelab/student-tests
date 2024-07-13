fe2c30e7e065312eef35296da1710a4a29a614a1
miha11-hash <d_mihaela@msn.com>
2024-07-14 01:29:11 +0300

    Profile (info) page

--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 function ProfilePage() {
   return (
-    <div>
-      Profile Page
+    <div className="flex align-center justify-center w-full h-full">
+      <div className="flex flex-col gap-y-4 px-4 text-xl">
+        <p className="text-xl">
+          Testul cuprinde 30 de intrebari. Fiecare intrebare are un singur
+          raspuns corect.{" "}
+        </p>
+
+        <p>
+          Fiecare raspuns corect este notat cu 0,4 puncte. Se acorda un punct
+          din oficiu.
+        </p>
+        <p>Timpul de lucru este de 2 ore.</p>
+        <p className="text-center pt-6">Succes! &#128522;</p>
+        <Button className="w-50 mt-6 mx-auto text-xl">
+          <Loader2 className="mr-2 animate-spin" />
+          <Link href="/profile/test">Incepe testul!</Link>
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProfilePage
--- app/page.js
+++ app/page.js
@@ -5,16 +5,16 @@ import Link from "next/link";
 function HomePage() {
   return (
     <div className="h-screen w-screen flex justify-center items-center background-div">
-      <div className="max-w-50 auto bg-slate-400 p-4">
-        <h1 className="text-center text-2xl font-semibold pb-6 tracking-wide">
+      <div className="max-w-50 auto p-10 rounded" style={{backgroundColor: "rgb(192, 192, 192, 0.6)"}}>
+        <h1 className="text-center text-2xl font-semibold pb-8 tracking-wide">
           Test
         </h1>
-        <h1 className="text-center text-2xl font-semibold pb-6 tracking-wide">
+        <h1 className="text-center text-2xl font-semibold pb-8 tracking-wide">
           Management Financiar
         </h1>
         <Link href="/profile">
-          <Button className="mx-auto block flex text-xl racking-wide hover:bg-green-600 transition duration-500">
-            <Loader2 className="mr-2" animate-spin /> Start Test
+          <Button className="mx-auto flex text-xl racking-wide hover:bg-green-600 transition duration-500">
+            <Loader2 className="mr-2 animate-spin" /> Start Test
           </Button>
         </Link>
       </div>
